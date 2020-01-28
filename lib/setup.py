801586f7df16b829e1b9063423f4bcd889f3ae53
LABESSE Maxence <Maxence.LABESSE@ag2rlamondiale.fr>
2020-01-28 13:38:05 +0100

    setup.py

---
+++ lib/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='AutoML',
+    version='1.0',
+    packages=[''],
+    url='',
+    license='',
+    author='Maxence LABESSE',
+    author_email='maxence.labesse@yahoo.fr',
+    description='Classification Automated Machine Learning'
+)
